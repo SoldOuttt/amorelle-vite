00b257ea41cb6bed19caa0a27501f412f5c4683a
SoldOuttt <ruzalkhafizov@yandex.ru>
2023-11-10 17:36:11 +0300

    feat: vite + react + ts + tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+    return <h1 className={'rounded bg-blue-500 px-4 py-2 text-base text-white'}>Hello world!</h1>;
+};
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
