c4d9a736b5b42e6080cd077d78763678c8227ba8
Ruzal <ruzalkhafizov@yandex.ru>
2023-11-11 17:14:33 +0300

    fix: svg to component

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './index.module.scss';
-import { Image } from '../../components/UI/Image';
-import { ImageWidthType } from '../../components/UI/Image/ImageWidthType.ts';
 import { Button } from '../../components/UI/Button';
 import { ButtonType } from '../../components/UI/Button/ButtonType.ts';
+import { Logo } from '../../../assets/icons/Logo';
+import { MascotLaugh } from '../../../assets/icons/MascotLaugh';
 
 const cx = classNames.bind(styles);
 
@@ -12,8 +12,8 @@ export const Landing: React.FC = () => {
     return (
         <div className={cx(styles.landing)}>
             <div className={cx('flex', 'flex-col', 'items-center', 'h-full', 'justify-between')}>
-                <Image alt={'logo'} src={'src/assets/logo.svg'} widthType={ImageWidthType.MAX} />
-                <Image alt={'mascot-laugh'} src={'src/assets/mascot-laugh.svg'} widthType={ImageWidthType.MAX} />
+                <Logo />
+                <MascotLaugh />
                 <div className={cx('flex', 'flex-col', 'gap-y-2.5', 'w-full')}>
                     <Button name={'Зарегистрироваться'} type={ButtonType.LIGHT_PURPLE} />
                     <Button name={'Войти'} type={ButtonType.LIGHT_PURPLE} />
