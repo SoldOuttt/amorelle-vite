a1b31e7de74d4fdf10affdfdfeb5caf1cb50d106
SoldOuttt <ruzalkhafizov@yandex.ru>
2024-01-19 20:47:36 +0300

    configure

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import classNames from 'classnames';
-import styles from './index.module.scss';
-import { Button } from '../../components/UI/Button';
-import { ButtonType } from '../../components/UI/Button/ButtonType.ts';
-import { Logo } from '../../../assets/icons/Logo';
-import { MascotLaugh } from '../../../assets/icons/MascotLaugh';
-
-const cx = classNames.bind(styles);
 
 export const Landing: React.FC = () => {
     return (
-        <div className={cx(styles.landing)}>
-            <div className={cx('flex', 'flex-col', 'items-center', 'gap-y-20')}>
-                <Logo />
-                <MascotLaugh />
-                <div className={cx('flex', 'flex-col', 'gap-y-2.5', 'w-full')}>
-                    <Button name={'Зарегистрироваться'} type={ButtonType.LIGHT_PURPLE} />
-                    <Button name={'Войти'} type={ButtonType.LIGHT_PURPLE} />
-                </div>
-            </div>
-        </div>
+        <>
+            <div className={'absolute left-0 top-0 h-[20px] w-[40px] border-2 bg-none'}></div>
+            <div>dfghjkjhgfd</div>
+        </>
     );
 };
